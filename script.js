2b8513be6d97c8c00c7ea5f1c5ab5e55caae9884
mpi-ag-lab <moulvibazarpolytechnicaglab@gmail.com>
2024-10-29 13:08:02 +0600

    added

---
+++ script.js
@@ -0,0 +1,48 @@
+// // variable var, let , const
+// /*
+// var a = 20
+// let b = 20
+// const c = 20
+// const d = 20;
+// */
+// // var ---> ager amoleer
+// console.log(a, b ,c,d)
+
+// b = 40;
+
+// let foysal_age = 20
+// let foysal_roll = 777323
+
+// let first_second_third_fourth = 10; //snake_case varibale
+// let firstSecondThirdFourth = 20; //camelCase varibale
+// let FirstSecondThirdFourth = 20 //PascalCase varibale
+
+// // d = 30
+// console.log(b)
+
+
+
+// console.log(a,b,c)
+
+
+let a; // variable declaration
+
+a = 20; // variable assigment
+a = 30 // variable reassignment
+
+let c = 20; //variable declaration and assignment
+// global variable and variable declare and assignment
+
+function abc(){
+    var num = 20
+    console.log(num)
+}
+// abc();
+
+
+// var problems
+// var redeclare 
+// global and function scope
+// it's can be access from outside of the scope
+
+console.log('this is outer function', num)
